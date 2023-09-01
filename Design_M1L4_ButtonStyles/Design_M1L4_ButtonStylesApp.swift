fcf192a40bf43dd7752a8ad873fb4eee0367f49d
Tom Montgomery <montgomery3681@gmail.com>
2023-08-31 22:10:30 -0600

    Initial Commit

---
+++ Design_M1L4_ButtonStyles/Design_M1L4_ButtonStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L4_ButtonStylesApp.swift
+//  Design_M1L4_ButtonStyles
+//
+//  Created by tom montgomery on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L4_ButtonStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
